c3323ce082f2c7ce9788557230b9b47bdb58359e
Huzaifa Amir <huzaifawha@gmail.com>
2026-07-31 20:03:25 +0100

    Added the airport class

---
+++ src/model/Airport.java
@@ -0,0 +1,32 @@
+package model;
+
+public class Airport {
+    private String code;
+    private String name;
+    private String city;
+
+    public Airport(String code, String name, String city) {
+        this.code = code;
+        this.name = name;
+        this.city = city;
+
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    @Override
+    public String toString() {
+        return (code + " - " + name + " (" + city + ")");
+    }
+
+}
